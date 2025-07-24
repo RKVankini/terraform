b337d6aaa9a78e581b5346b5578bd7b4e21c20a5
ramakrishna <ramakrishnavankini32@gmail.com>
2025-07-24 16:47:44 +0530

    10tenth-day-modules-source-terraform

---
+++ 10tenth-day-modules-source-terraform/terraform.tfvars
@@ -0,0 +1,4 @@
+bucket = "RK_terra_bucket"
+acl = "private"
+control_object_ownership = true
+object_ownership = "ObjectWriter"
---
+++ 10tenth-day-modules-source-terraform/variables.tf
@@ -0,0 +1,16 @@
+variable "bucket" {
+    type = string
+    default = ""
+}
+variable "acl" {
+    type = string
+    default = ""
+}
+variable "control_object_ownership" {
+    default = ""
+  
+}
+variable "object_ownership" {
+    default = ""
+  
+}
