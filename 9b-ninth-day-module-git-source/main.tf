4ccf265dfff65a1559e7fad7c5c9a4ec24f824f2
RKVankini <ramakrishnavankini32@gmail.com>
2025-07-24 13:39:05 +0530

    Update main.tf

--- 9b-ninth-day-module-git-source/main.tf
+++ 9b-ninth-day-module-git-source/main.tf
@@ -1,5 +1,5 @@
 module "prod" {
-    source = "github.com/RKVankini/terraform/9ninth-day-modules"
+    #source = "github.com/RKVankini/terraform/9ninth-day-modules"
     ami_id = var.ami_id
     instance_type = var.instance_type
     key = var.key
